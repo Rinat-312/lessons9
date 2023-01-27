424c393390122cf84afd88b833ec2c43763a00b5
Rinat-312 <rkozhobekov88@mail.ru>
2023-01-27 14:36:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+public class Main {
+    public static void main(String[] args) {
+//        myFirstReturn();
+//        System.out.println();
+//        int n = one();
+//        System.out.println(n + 1);
+
+//        int a = san(new int[]{3, 7, 8, 9});
+//        System.out.println(a);
+
+//        System.out.println("chislo  " + a);
+
+//        String name = findUser("Aiperi");
+//        System.out.println(name);
+
+        String a = method(122333);
+        System.out.println(a);
+
+    }
+
+    //    static double method(int a, int b, int c) {
+//        return (a + b + c) / 3d;
+//
+//
+//
+//        static String myMethod(String name) {
+//        if (name.length() <= 4) {
+//            return "atynyz kyska";
+//        } else if (name.length() <= 7) {
+//            return "atynyz ortocho";
+//        } else {"
+//            return "aatynyz uzun"*//*
+//
+//    static int san(int[] array) {
+//
+//        int max = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (max < array[i]) {
+//                max = array[i];
+//
+//
+//            }
+//
+//        }
+//        return + max;
+
+
+    static String method(int a) {
+        if (a % 2 == 0) {
+            return "jup san";
+        } else {
+            return "tak san";
+
+        }
+    }
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+////    static String findUser(String userName) {
+////        String[] array = {"Aiperi", "Azamat", "Krasavchic"};
+////
+////        for (int i = 0; i < array.length; i++) {
+////
+////            if (array[i] == userName) {
+////            return "User with username:" + userName + " succsefully!";
+////        }
+////    }
+////    return " User "+ userName + " found";
+////}
+//}
+
+
+//
+
+
+//
+//
+
+
+//    static int one() {
+//        return 1;
+//
+//
+//
+
+
